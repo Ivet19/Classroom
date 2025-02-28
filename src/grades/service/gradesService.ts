a893c2dc4479346f2b365a547fb68fca688b03cc
Ivet Lopez <ivet19@live.com>
2025-02-28 21:09:59 +0100

    Create addGrade function

--- src/grades/service/gradesService.ts
+++ src/grades/service/gradesService.ts
@@ -21,4 +21,27 @@ export const getGradesTotal = (grades: Grade[]): number => {
 // Crea una función para crear una nueva nota
 // La función debe recibir un array de notas, el id del estudiante, el id del curso y el valor de la nota
 // Si la nota ya existe, muestra un error con showErrorModal
-// export const addGrade =
+export const addGrade = (
+  grades: Grade[],
+  studentId: number,
+  courseId: number,
+  number: number
+): void => {
+  const newGrade: Grade = {
+    id: generateId(grades),
+    studentId: studentId,
+    courseId: courseId,
+    value: number,
+  };
+
+  if (
+    grades.some(
+      (grade) => grade.studentId === studentId && grade.courseId === courseId
+    )
+  ) {
+    showErrorModal("Ya existe una nota para este estudiante de este curso");
+    return;
+  }
+
+  grades.push(newGrade);
+};
