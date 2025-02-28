f90165a78f9af90ca2d01889f78131b91b532db2
Ivet Lopez <ivet19@live.com>
2025-03-01 00:01:58 +0100

    Create getGradeFullData function

--- src/grades/service/gradesService.ts
+++ src/grades/service/gradesService.ts
@@ -12,15 +12,37 @@ export const getGradesTotal = (grades: Grade[]): number => {
 // La función debe recibir una nota
 // La función debe devolver un objeto con las mismas propiedades de la nota
 // más las propiedades studentName, studentLastName y courseName
-// export const getGradeFullData =
+
+type GradeFullData = {
+  id: number;
+  studentId: number;
+  studentName: string;
+  studentLastName: string;
+  courseId: number;
+  courseName: string;
+  value: number;
+};
+
+export const getGradeFullData = (grade: Grade): GradeFullData => {
+  const student = students.find((student) => student.id === grade.studentId);
+  const studentName = student?.name;
+  const studentLastName = student?.lastName;
+  const course = courses.find((course) => course.id === grade.courseId);
+  const courseName = course?.name;
+
+  const gradeFullData: GradeFullData = Object.assign(grade, {
+    studentName: studentName!,
+    studentLastName: studentLastName!,
+    courseName: courseName!,
+  });
+
+  return gradeFullData;
+};
 
 // Crea una función para eliminar una nota de la lista de notas
 // La función debe recibir un array de notas y el id de la nota a eliminar
 // export const deleteGrade =
 
-// Crea una función para crear una nueva nota
-// La función debe recibir un array de notas, el id del estudiante, el id del curso y el valor de la nota
-// Si la nota ya existe, muestra un error con showErrorModal
 export const addGrade = (
   grades: Grade[],
   studentId: number,
