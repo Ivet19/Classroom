50eca433e541885e3b36b2265165d845e526f109
Ivet Lopez <ivet19@live.com>
2025-03-01 01:24:16 +0100

    Create getCourseStats function to calculate the statics

--- src/stats/service/statsService.ts
+++ src/stats/service/statsService.ts
@@ -1,7 +1,38 @@
 import { courses, grades } from "../../index.js";
 import { CourseStats } from "../../types";
 
-// Crea una función para obtener las estadísticas de un curso
-// La función debe recibir el id de un curso
-// La función debe devolver un objeto de tipo CourseStats
-// export const getCourseStats =
+export const getCourseStats = (courseId: number): CourseStats => {
+  const courseGrades = grades.filter((grade) => grade.courseId === courseId);
+
+  const studentsCount = courseGrades.length;
+  const passedCount = courseGrades.filter((grade) => grade.value >= 5).length;
+  const failedCount = courseGrades.filter((grade) => grade.value < 5).length;
+
+  const passedCountPercentage = (passedCount / studentsCount) * 100;
+  const failedCountPercentage = (failedCount / studentsCount) * 100;
+
+  const averageGrade =
+    courseGrades.reduce((sum, grade) => sum + grade.value, 0) /
+    courseGrades.length;
+
+  const gradesValues = courseGrades.map((grade) => grade.value);
+  const highestGradeValue = Math.max(...gradesValues);
+  const highestGrade = courseGrades.find(
+    (grade) => grade.value === highestGradeValue
+  );
+  const highestGradeStudentId = highestGrade?.studentId;
+
+  const courseStats: CourseStats = {
+    courseId: courseId,
+    studentsCount: studentsCount,
+    passedCount: passedCount,
+    passedCountPercentage: passedCountPercentage,
+    failedCount: failedCount,
+    failedCountPercentage: failedCountPercentage,
+    averageGrade: averageGrade,
+    highestGrade: highestGradeValue,
+    highestGradeStudentId: highestGradeStudentId!,
+  };
+
+  return courseStats;
+};
